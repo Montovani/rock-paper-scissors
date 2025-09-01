41f19f0864b97b7b815426ee155dc947384ce40d
Igor Montovani <igor.montovani@gmail.com>
2025-09-01 11:21:57 +0200

    Add function to get userchoice, generate computer choice and run the game to see the winner.

--- js.js
+++ js.js
@@ -1,15 +1,52 @@
-let playerScore = 0
+let userChoice = ''
+let computerChoice = ''
+let userScore = 0
 let computerScore = 0
-let btnChooseRock = document.querySelector(".btn-choose-rock")
-const btnChoosePaper = document.querySelector(".btn-choose-paper")
-const btnChooseScissors = document.querySelector(".btn-choose-scissors")
-let computerChoice = 'Paper'
-let userChoice = getUserChoice()
 
-function getUserChoice () {
-btnChooseRock.addEventListener('click', () => {userChoice = 'Rock'})
-btnChoosePaper.addEventListener('click', ()=> {userChoice = 'Paper'})
-btnChooseScissors.addEventListener('click', ()=> {userChoice = 'Scissors'})
+
+const btnRock = document.querySelector('.btn-rock')
+const btnPaper = document.querySelector('.btn-paper')
+const btnScissors = document.querySelector('.btn-scissors')
+
+//Event to get the user choice and trigger the computer choice
+btnRock.addEventListener('click',getUserChoice)
+btnRock.addEventListener('click',getComputerChoice)
+btnRock.addEventListener('click',gameMatch)
+btnPaper.addEventListener('click',getUserChoice)
+btnPaper.addEventListener('click',getComputerChoice)
+btnPaper.addEventListener('click',gameMatch)
+btnScissors.addEventListener('click',getUserChoice)
+btnScissors.addEventListener('click',getComputerChoice)
+btnScissors.addEventListener('click',gameMatch)
+
+function getUserChoice (event) {
+    if (event.target.id === 'rock') {
+        userChoice = 'Rock'
+    } else if (event.target.id === 'paper') {
+        userChoice = 'Paper'
+    } else if (event.target.id === 'scissors') {
+        userChoice = 'Scissors'
+    }
+    return console.log(userChoice)
+}    
+function getComputerChoice () {
+    const computerOptions = ['Rock','Paper','Scissors']
+    const randomChoice = Math.floor(Math.random() * computerOptions.length)
+    return console.log(computerChoice = computerOptions[randomChoice])
 }
-
-
+function gameMatch() {
+    if (userChoice === 'Rock' && computerChoice === 'Paper') {
+        computerScore++
+    }else if (userChoice === 'Rock' && computerChoice === 'Scissors') {
+        userScore++
+    } else if (userChoice === 'Paper' && computerChoice === 'Rock'){
+        userScore++
+    } else if (userChoice === 'Paper' && computerChoice === 'Scissors'){
+        computerScore++
+    } else if (userChoice === 'Scissors' && computerChoice === 'Paper'){
+        userScore++
+    } else if (userChoice === 'Scissors' && computerChoice === 'Rock'){
+        computerScore++
+    }
+    console.log(`User Score: ${userScore}  - Computer Score: ${computerScore}`)
+}
